51cf6fe354b8980d1a621535fe48b4f0f0116167
MarinaCarbonell <marinacarbonellalvarez@gmail.com>
2020-08-23 19:16:06 +0200

    adding landing styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../stylesheets/App.css';
+import '../stylesheets/App.scss';
 import './Landing';
 import Landing from './Landing';
 import {Switch, Route} from 'react-router-dom';
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import '../stylesheets/Landing.scss';
 import { Link } from 'react-router-dom';
 
 function Landing() {
     return (
       <div className="landing">
-        <p>Marina Carbonell</p>
-        <button className="english-button">
+        <p>Marina Carbonell Álvarez.</p>
+        <div>
+          <button className="button">
             <Link to="/hello">Hello!</Link>
-        </button>
-        <button className="spanish-button">
+          </button>
+          <button className="button">
             <Link to="/hola">¡Hola!</Link>
-        </button>
+          </button>
+        </div>
       </div>
              
   
