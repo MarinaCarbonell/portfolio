fdc16bc12e0a1a3f330bd6b50c701d295c80e505
MarinaCarbonell <marinacarbonellalvarez@gmail.com>
2020-09-24 19:30:18 +0200

    adding fixed animation

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,20 +7,20 @@ function Landing(props) {
       <div className="wrapper">
         <div className="landing-wrapper">
           <div className="text">
-            <p className="p">¡Hola! Soy Marina y hasta ahora he trabajado en <em>Marketing Digital.</em></p> 
-            <p className="p">Como el mundo de web y de la programación me ha encantado</p>
-            <p className="p">decidí formarme como <em>Programadora Front-End</em> en <a href="https://adalab.es/">Adalab</a>.</p>
-            <p className="p">Donde realicé los siguientes <Link to="/proyects">Proyectos</Link></p>
+            <p className="p p1">¡Hola! Soy Marina y hasta ahora he trabajado en <em>Marketing Digital.</em></p> 
+            <p className="p p2">Como el mundo de web y de la programación me ha encantado</p>
+            <p className="p p3">decidí formarme como <em>Programadora Front-End</em> en <a href="https://adalab.es/">Adalab</a>.</p>
+            <p className="p p4">Donde realicé los siguientes <Link to="/proyects">Proyectos</Link></p>  
+            <p className="p p5">
+              Soy <em>aprendiz</em> incansable, cotilla del <em>código ajeno</em> y <em>lectora</em> de todo lo que cae ante mis ojos.
+            </p>
+            <p className="p p6">
+             Si quieres saber más sobre mí, no dudes en visitar mi <Link to="/curriculum">Curríulum</Link>.
+            </p>
+            <p className="p p7">
+              Si quieres ponerte en <Link to="/contact">contacto</Link> conmigo hazlo a través del
+            </p>
           </div>
-          <p className="text">
-            Soy <em>aprendiz</em> incansable, cotilla del <em>código ajeno</em> y <em>lectora</em> de todo lo que cae ante mis ojos.
-          </p>
-          <p className="text">
-            Si quieres saber más sobre mí, no dudes en visitar mi <Link to="/curriculum">Curríulum</Link>.
-          </p>
-          <p className="text">
-            Si quieres ponerte en <Link to="/contact">contacto</Link> conmigo hazlo a través del
-          </p>
         </div>  
       </div>
     );
