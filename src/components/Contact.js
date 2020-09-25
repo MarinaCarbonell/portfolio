13a04134829998f4d1f5e18b8bfe83b5ad827454
MarinaCarbonell <marinacarbonellalvarez@gmail.com>
2020-09-25 12:52:34 +0200

    adding css changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,34 +1,59 @@
-import React from 'react';
-import '../stylesheets/Contact.scss';
-import { Link } from 'react-router-dom';
-import image from '../docs/images/M.png';
-
+import React from "react";
+import "../stylesheets/Contact.scss";
+import { Link } from "react-router-dom";
+import image from "../docs/images/M.png";
 
 function Contact() {
-
   return (
     <div className="wrapper">
       <div className="contact">
-      <Link to="/">
-        <img className="profile-img" src={image} alt="profile-img"></img>
-      </Link>
-        <h2 className="title">¿Quieres ponerte en contacto conmigo?</h2>
-          <p className="contact__phrase">
-            Puedes hacerlo rellenando el siguiente formulario
-          </p>
-          <form action="https://formspree.io/xnqgklry" method="post" className="contact__form">
-            <label for="fullName" className="contact__form-label">Nombre</label>
-            <input className="contact__form-input" type="text" id="fullName" name="fullName" placeholder="Tu nombre" required />
-            <label for="email" className="contact__form-label">Email</label>
-            <input className="contact__form-input" type="email" id="email" name="email" placeholder="tuemail@mail.com" required />
-            <label for="message" className="contact__form-label">Mensaje</label>
-            <textarea className="contact__form-input" id="message" name="message" placeholder="Escríbeme lo que necesites"required></textarea>
-            <input className="button_send" type="submit" value="Enviar"/>
-            <input className="button_thanks" type="submit" value="¡Gracias!"/>          
-          </form>
+        <Link to="/">
+          <img className="profile-img" src={image} alt="profile-img"></img>
+        </Link>
+        <h2 className="title">Formulario de contacto</h2>
+        <form
+          action="https://formspree.io/xnqgklry"
+          method="post"
+          className="contact__form"
+        >
+          <label for="fullName" className="contact__form-label">
+            Nombre
+          </label>
+          <input
+            className="contact__form-input"
+            type="text"
+            id="fullName"
+            name="fullName"
+            placeholder="Tu nombre"
+            required
+          />
+          <label for="email" className="contact__form-label">
+            Email
+          </label>
+          <input
+            className="contact__form-input"
+            type="email"
+            id="email"
+            name="email"
+            placeholder="tuemail@mail.com"
+            required
+          />
+          <label for="message" className="contact__form-label">
+            Mensaje
+          </label>
+          <textarea
+            className="contact__form-input"
+            id="message"
+            name="message"
+            placeholder="Escríbeme lo que necesites"
+            required
+          ></textarea>
+          <input className="button_send" type="submit" value="Enviar" />
+          <input className="button_thanks" type="submit" value="¡Gracias!" />
+        </form>
       </div>
     </div>
-    );
-  }
+  );
+}
 
 export default Contact;
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,29 +1,56 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../stylesheets/Landing.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../stylesheets/Landing.scss";
 
 function Landing(props) {
-    return (
-      <div className="wrapper">
-        <div className="landing-wrapper">
-          <div className="text">
-            <p className="p p1">¡Hola! Soy Marina y hasta ahora he trabajado en <em>Marketing Digital.</em></p> 
-            <p className="p p2">Como el mundo de web y de la programación me ha encantado</p>
-            <p className="p p3">decidí formarme como <em>Programadora Front-End</em> en <a href="https://adalab.es/">Adalab</a>.</p>
-            <p className="p p4">Donde realicé los siguientes <Link to="/proyects">Proyectos</Link></p>  
-            <p className="p p5">
-              Soy <em>aprendiz</em> incansable, cotilla del <em>código ajeno</em> y <em>lectora</em> de todo lo que cae ante mis ojos.
-            </p>
-            <p className="p p6">
-             Si quieres saber más sobre mí, no dudes en visitar mi <Link to="/curriculum">Curríulum</Link>.
-            </p>
-            <p className="p p7">
-              Si quieres ponerte en <Link to="/contact">contacto</Link> conmigo hazlo a través del
-            </p>
-          </div>
-        </div>  
+  return (
+    <div className="wrapper">
+      <div className="landing-wrapper">
+        <div className="text">
+          <p className="p p1">
+            ¡Hola! Soy Marina y hasta ahora he trabajado en{" "}
+            <em>Marketing Digital.</em>
+          </p>
+          <p className="p p2">
+            Como el mundo web y de la programación siempre me ha encantado
+          </p>
+          <p className="p p3">
+            decidí formarme como <em>Programadora Front-End</em> en{" "}
+            <a href="https://adalab.es/">Adalab</a>,
+          </p>
+          <p className="p p4">
+            donde realicé los siguientes{" "}
+            <Link to="/proyects">
+              <span className="underline">Proyectos</span>.
+            </Link>
+          </p>
+          <p className="p p5">
+            Soy <em>aprendiz</em> incansable, cotilla del <em>código ajeno</em>{" "}
+            y <em>lectora</em> de todo lo
+          </p>
+          <p className="p p6">que cae ante mis ojos.</p>
+          <p className="p p7">
+            Si quieres saber más sobre mí, no dudes en visitar mi{" "}
+            <Link to="/curriculum">
+              <span className="underline">Currículum</span>.
+            </Link>
+          </p>
+          <p className="p p8">
+            Para ponerte en contacto conmigo, puedes hacerlo a través del
+          </p>
+          <p className="p p9">
+            formulario que encontrarás haciendo click{" "}
+            <Link to="/contact">
+              <span className="underline">aquí</span>.
+            </Link>
+          </p>
+          <p className="p p10">
+            <i className="far fa-smile"></i>
+          </p>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default Landing;
