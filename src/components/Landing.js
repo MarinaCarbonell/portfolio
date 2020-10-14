235bb59c26464b96d9f0d0a781d0ec150355aa4b
MarinaCarbonell <marinacarbonellalvarez@gmail.com>
2020-10-14 12:26:48 +0200

    adding mediaqueries

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,6 +7,9 @@ function Landing(props) {
   const isDesktop = useMediaQuery({
     query: "(min-device-width: 1024px)",
   });
+  const isTablet = useMediaQuery({
+    query: "(min-device-width: 768px)",
+  });
   return (
     <div className="wrapper">
       <div className="landing-wrapper">
@@ -34,7 +37,7 @@ function Landing(props) {
                 Soy <em>aprendiz</em> incansable, cotilla del{" "}
                 <em>código ajeno</em> y <em>lectora</em> de todo lo
               </p>
-              <p className="p p6">que cae en mis ojos.</p>
+              <p className="p p6">que cae en mis manos.</p>
               <p className="p p7">
                 Si quieres saber más sobre mí, no dudes en visitar mi{" "}
                 <Link to="/curriculum">
@@ -56,7 +59,7 @@ function Landing(props) {
             </div>
           </>
         )}
-        {!isDesktop && (
+        {isTablet && !isDesktop && (
           <>
             <div className="text">
               <p className="p p1">¡Hola! Soy Marina</p>
@@ -83,7 +86,7 @@ function Landing(props) {
               <p className="p p10">
                 <em>código ajeno</em> y <em>lectora</em>
               </p>
-              <p className="p p11">de todo lo que cae en mis ojos.</p>
+              <p className="p p11">de todo lo que cae en mis manos.</p>
               <p className="p p12">Si quieres saber más sobre mí,</p>
               <p className="p p13">
                 no dudes en visitar mi{" "}
@@ -95,6 +98,63 @@ function Landing(props) {
               <p className="p p15">puedes hacerlo a través del</p>
               <p className="p p16">formulario que encontrarás </p>
               <p className="p p17">
+                haciendo click{" "}
+                <Link to="/contact">
+                  <span className="underline">aquí</span>.
+                </Link>
+              </p>
+              <p>
+                <i className="far fa-smile"></i>
+              </p>
+            </div>
+          </>
+        )}
+        {!isTablet && !isDesktop && (
+          <>
+            <div className="text">
+              <p className="p p1">¡Hola! Soy Marina</p>
+              <p className="p p2">y he trabajado en</p>
+              <p className="p p3">
+                <em>Marketing Digital.</em>
+              </p>
+              <p className="p p4">Como el mundo web</p>
+              <p className="p p5">y de la programación</p>
+              <p className="p p6">siempre me ha encantado</p>
+              <p className="p p7">decidí formarme como</p>
+              <p className="p p8">
+                <em>Programadora Front-End</em>
+              </p>
+              <p className="p p9">
+                en <a href="https://adalab.es/">Adalab</a>,
+              </p>
+              <p className="p p10">donde realicé los</p>
+              <p className="p p11">
+                siguientes{" "}
+                <Link to="/proyects">
+                  <span className="underline">Proyectos</span>.
+                </Link>
+              </p>
+              <p className="p p12">
+                Soy <em>aprendiz</em> incansable,
+              </p>
+              <p className="p p13">
+                cotilla del <em>código ajeno</em>{" "}
+              </p>
+              <p className="p p14">
+                y <em>lectora</em> de todo
+              </p>
+              <p className="p p15">lo que cae en mis manos.</p>
+              <p className="p p16">Si quieres saber más,</p>
+              <p className="p p17">
+                visita mi{" "}
+                <Link to="/curriculum">
+                  <span className="underline">Currículum</span>.
+                </Link>
+              </p>
+              <p className="p p18">Para ponerte en contacto</p>
+              <p className="p p19">conmigo, hazlo a través</p>
+              <p className="p p20">del formulario </p>
+              <p className="p p21">
                 haciendo click{" "}
                 <Link to="/contact">
                   <span className="underline">aquí</span>.
